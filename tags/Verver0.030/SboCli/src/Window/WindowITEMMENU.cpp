ac8a1b66a98c63ab0f91265c580199fac54ed4c4
RXP10430 <RXP10430@15353e6c-9749-11dd-b35e-65caead18e0b>
2008-11-28 12:25:47 +0000

    2008/11/28版(ver0.030) ・マップ移動時にマップ名を表示するように実装。 ・Vキーはアイテム名の表示切り替えをするように修正。 ・NPC会話処理を実装。 ・マップ移動時のマップ名表示のフォントと表示位置などを調整。 ・会話ウィンドウの枠の角が黒くなっていたのを修正。 ・マップ名が表示されている時にマップ移動すると表示をキャンセルするように修正。 ・[管理者ウィンドウ]キャラ情報の設定に色設定の項目を追加。 ・マップ名表示で右端が欠けることがあったのを修正。 ・アイテムのドラッグ中は半透明で表示するように修正。 ・マップイベントで向きによる判定ができるように対応。 ・マップ内移動とマップ間移動で、移動後の向きが設定できるように対応。 ・縦に視点移動すると、キャラが見えないことがあったのを修正。 ・会話ウィンドウで名前用の枠を表示するように対応。 ・移動先のマップが同じBGMだった場合は再生し直さないように修正。 ・ついていく相手がマップイベント上にいる時はついていけないように修正。 ・スクロール時に重ね合わせマップパーツがキャラに重ならないことがあったのを修正。 ・[管理者ウィンドウ]マップウィンドウにグリッド表示できるように対応。 ・[管理者ウィンドウ]キャラ情報表示中でマップ切り替えした時にキャラ情報が消えていたのを修正。 ・[管理者ウィンドウ]キャラ情報画面からキャラを削除できるように対応。 ・会話データから選択項目を表示できるように対応。 ・BGMを3曲追加

---
+++ tags/Verver0.030/SboCli/src/Window/WindowITEMMENU.cpp
@@ -0,0 +1,509 @@
+/* Copyright(C)URARA-works 2007 */
+/* ========================================================================= */
+/* ファイル名	:WindowITEMMENU.cpp											 */
+/* 内容			:アイテムメニューウィンドウクラス 実装ファイル				 */
+/* 作成			:年がら年中春うらら(URARA-works)							 */
+/* 作成開始日	:2007/07/30													 */
+/* ========================================================================= */
+
+#include "stdafx.h"
+#include "LibInfoItem.h"
+#include "InfoCharCli.h"
+#include "Img32.h"
+#include "MgrData.h"
+#include "MgrDraw.h"
+#include "MgrGrpData.h"
+#include "MgrWindow.h"
+#include "MgrSound.h"
+#include "WindowITEMMENU.h"
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::CWindowITEMMENU								 */
+/* 内容		:コンストラクタ													 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+CWindowITEMMENU::CWindowITEMMENU()
+{
+	m_ptDrag.x = m_ptDrag.y = -1;
+	m_nPos			= EQUIPTYPE_MAX + 12;
+	m_nPosMax		= EQUIPTYPE_MAX + 24;
+	m_bInput		= TRUE;
+	m_nID			= WINDOWTYPE_ITEMMENU;
+	m_ptViewPos.x	= 24;
+	m_ptViewPos.y	= 64;
+	m_sizeWindow.cx	= 186;
+	m_sizeWindow.cy	= 301;
+
+	m_dwSelectItemID = 0;
+	m_dwDragItemID	 = 0;
+
+	m_pPlayerChar	= NULL;
+	m_pLibInfoItem	= NULL;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::~CWindowITEMMENU								 */
+/* 内容		:デストラクタ													 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+CWindowITEMMENU::~CWindowITEMMENU()
+{
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::Create										 */
+/* 内容		:作成															 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+void CWindowITEMMENU::Create(CMgrData *pMgrData)
+{
+	CWindowBase::Create (pMgrData);
+
+	m_bActive = TRUE;
+	m_pDib->Create (m_sizeWindow.cx, m_sizeWindow.cy);
+	m_pDib->SetColorKey (0);
+
+	m_pPlayerChar	= m_pMgrData->GetPlayerChar ();
+	m_pLibInfoItem	= m_pMgrData->GetLibInfoItem ();
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::Draw											 */
+/* 内容		:描画															 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+void CWindowITEMMENU::Draw(PCImg32 pDst)
+{
+	int i, nCount, nLevel, x, y, nCursor;
+	HDC hDC;
+	HFONT hFontOld;
+	PCInfoItem pInfoItem, pIntoItemDrag;
+
+	if (m_dwTimeDrawStart) {
+		goto Exit;
+	}
+
+	m_strName.Empty ();
+	m_pDib->BltFrom256 (186 / 2 - 62, 0, 124, 84, m_pDibSystem, 368, 200, TRUE);
+	m_pDib->BltFrom256 (0, 84, 186, 217, m_pDibSystem, 0, 200, TRUE);
+
+	/* 装備アイテムを描画 */
+	DrawEquip (0, m_pPlayerChar->m_dwEquipItemIDCloth);		/* 服 */
+	DrawEquip (1, m_pPlayerChar->m_dwEquipItemIDAcce1);		/* アクセサリ1 */
+	DrawEquip (2, m_pPlayerChar->m_dwEquipItemIDArmsRight);	/* 持ち物 */
+	DrawEquip (3, m_pPlayerChar->m_dwEquipItemIDArmsLeft);	/* 盾 */
+
+	pIntoItemDrag = NULL;
+
+	/* アイテムを描画 */
+	nCount = m_pPlayerChar->m_adwItemID.GetSize ();
+	for (i = 0; i < nCount; i ++) {
+		pInfoItem = (PCInfoItem)m_pLibInfoItem->GetPtr (m_pPlayerChar->m_adwItemID[i]);
+		if (pInfoItem == NULL) {
+			continue;
+		}
+		x = pInfoItem->m_ptBackPack.x;
+		y = pInfoItem->m_ptBackPack.y;
+		if ((m_ptDrag.x == x) && (m_ptDrag.y == y)) {
+			pIntoItemDrag = pInfoItem;
+		}
+		m_pMgrDraw->DrawItem (m_pDib, 10 + (x * 33), 95 + (y * 33), pInfoItem);
+		if (m_strName.IsEmpty () == FALSE) {
+			continue;
+		}
+		if (((m_nPos - EQUIPTYPE_MAX) % 5 == x) &&
+			((m_nPos - EQUIPTYPE_MAX) / 5 == y)) {
+			m_strName = pInfoItem->m_strName;
+		}
+	}
+
+	/* ドラッグ中？ */
+	if (pIntoItemDrag) {
+		x = (m_nPos - EQUIPTYPE_MAX) % 5;
+		y = (m_nPos - EQUIPTYPE_MAX) / 5;
+		m_pMgrDraw->DrawItem (m_pDib, 10 + (x * 33), 95 + (y * 33), pIntoItemDrag, 50);
+	}
+
+	GetDrawPos (m_nPos, x, y);
+
+	nCursor = (m_ptDrag.x == -1) ? 2 : 3;
+	m_pMgrDraw->DrawCursor (m_pDib, x - 8, y, nCursor);
+	m_dwTimeDrawStart = timeGetTime ();
+
+Exit:
+	nLevel = 100;
+	if (m_bActive == FALSE) {
+		nLevel = 60;
+	}
+	pDst->BltLevel (m_ptViewPos.x + 32, m_ptViewPos.y + 32, m_sizeWindow.cx, m_sizeWindow.cy, m_pDib, 0, 0, nLevel, TRUE);
+
+	/* アイテム名を表示 */
+	if (m_strName.IsEmpty () == FALSE) {
+		hDC			= pDst->Lock ();
+		hFontOld	= (HFONT)SelectObject (hDC, m_hFont12);
+		SetBkMode (hDC, TRANSPARENT);
+
+		GetDrawPos (m_nPos, x, y);
+		x = m_ptViewPos.x + 32 + x - 8;
+		y = m_ptViewPos.y + 32 + y - 24;
+		DrawFrame2 (x, y, m_strName.GetLength () * 6, 16, 0, pDst, 4);
+		TextOut2 (hDC, x, y, (LPCSTR)m_strName, RGB (10, 10, 10), FALSE);
+
+		SelectObject (hDC, hFontOld);
+		pDst->Unlock ();
+	}
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::DragOff										 */
+/* 内容		:ドラッグ解除													 */
+/* 日付		:2008/11/22														 */
+/* ========================================================================= */
+
+void CWindowITEMMENU::DragOff(void)
+{
+	m_ptDrag.x = m_ptDrag.y = -1;
+	m_dwDragItemID = 0;
+	Redraw ();
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnUp											 */
+/* 内容		:キーハンドラ(↑)												 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnUp(void)
+{
+	BOOL bRet;
+
+	bRet = FALSE;
+
+	if (m_nPos < EQUIPTYPE_MAX) {
+		if (m_nPos < 3) {
+			goto Exit;
+		}
+		m_nPos -= 3;
+	} else {
+		if (m_nPos < 10) {
+			switch (m_nPos) {
+			case 6:	m_nPos = 3;	break;
+			case 7:	m_nPos = 4;	break;
+			case 8:	m_nPos = 4;	break;
+			case 9:	m_nPos = 5;	break;
+			}
+		} else {
+			m_nPos -= 5;
+		}
+	}
+	if (m_nPos < EQUIPTYPE_MAX) {
+		/* ドラッグ解除 */
+		DragOff ();
+	}
+
+	m_nCursorAnime = 0;
+	m_dwLastTimeCursor = 0;
+	m_pMgrSound->PlaySound (SOUNDID_CURSORMOVE);
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnDown										 */
+/* 内容		:キーハンドラ(↓)												 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnDown(void)
+{
+	BOOL bRet;
+
+	bRet = FALSE;
+
+	if (m_nPos < EQUIPTYPE_MAX) {
+		if (m_nPos < 3) {
+			m_nPos += 3;
+		} else {
+			switch (m_nPos) {
+			case 3:	m_nPos = 6;	break;
+			case 4:	m_nPos = 7;	break;
+			case 5:	m_nPos = 8;	break;
+			}
+		}
+	} else {
+		if (m_nPos >= m_nPosMax - 4) {
+			goto Exit;
+		}
+		m_nPos += 5;
+	}
+
+	m_nCursorAnime = 0;
+	m_dwLastTimeCursor = 0;
+	m_pMgrSound->PlaySound (SOUNDID_CURSORMOVE);
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnLeft										 */
+/* 内容		:キーハンドラ(←)												 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnLeft(void)
+{
+	BOOL bRet;
+
+	bRet = FALSE;
+
+	if (m_nPos < EQUIPTYPE_MAX) {
+		if (m_nPos % 3 == 0) {
+			goto Exit;
+		}
+	} else {
+		if ((m_nPos - EQUIPTYPE_MAX) % 5 == 0) {
+			goto Exit;
+		}
+	}
+	m_nPos --;
+	m_nCursorAnime = 0;
+	m_dwLastTimeCursor = 0;
+	m_pMgrSound->PlaySound (SOUNDID_CURSORMOVE);
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnRight										 */
+/* 内容		:キーハンドラ(→)												 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnRight(void)
+{
+	BOOL bRet;
+
+	bRet = FALSE;
+
+	if (m_nPos < EQUIPTYPE_MAX) {
+		if (m_nPos % 3 == 2) {
+			goto Exit;
+		}
+	} else {
+		if ((m_nPos - EQUIPTYPE_MAX) % 5 == 4) {
+			goto Exit;
+		}
+	}
+	m_nPos ++;
+	m_nCursorAnime = 0;
+	m_dwLastTimeCursor = 0;
+	m_pMgrSound->PlaySound (SOUNDID_CURSORMOVE);
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnB											 */
+/* 内容		:キーハンドラ(B)												 */
+/* 日付		:2007/09/09														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnB(BOOL bDown)
+{
+	return OnZ (bDown);
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnX											 */
+/* 内容		:キーハンドラ(X)												 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnX(BOOL bDown)
+{
+	int i, nCount;
+	POINT ptPos;
+	BOOL bRet;
+	DWORD dwItemID, dwItemIDDrag;
+	PCInfoItem pInfoItem;
+
+	bRet		 = FALSE;
+	dwItemID	 = 0;
+	dwItemIDDrag = 0;
+
+	ptPos.x = (m_nPos - EQUIPTYPE_MAX) % 5;
+	ptPos.y = (m_nPos - EQUIPTYPE_MAX) / 5;
+
+	if (bDown) {
+		if (m_ptDrag.x == -1) {
+			m_ptDrag = ptPos;
+			Redraw ();
+		}
+		goto Exit;
+	}
+	if ((m_ptDrag.x == ptPos.x) && (m_ptDrag.y == ptPos.y)) {
+		DragOff ();
+	}
+
+	if (m_nPos < EQUIPTYPE_MAX) {
+		switch (m_nPos) {
+		case EQUIPTYPE_ACCE1:		dwItemID = m_pPlayerChar->m_dwEquipItemIDAcce1;		break;	/* アクセサリ1 */
+		case EQUIPTYPE_HEAD:		dwItemID = m_pPlayerChar->m_dwEquipItemIDHead;		break;	/* 頭 */
+		case EQUIPTYPE_ACCE2:		dwItemID = m_pPlayerChar->m_dwEquipItemIDAcce2;		break;	/* アクセサリ2 */
+		case EQUIPTYPE_ARMSRIGHT:	dwItemID = m_pPlayerChar->m_dwEquipItemIDArmsRight;	break;	/* 右手 */
+		case EQUIPTYPE_CLOTH:		dwItemID = m_pPlayerChar->m_dwEquipItemIDCloth;		break;	/* 服 */
+		case EQUIPTYPE_ARMSLEFT:	dwItemID = m_pPlayerChar->m_dwEquipItemIDArmsLeft;	break;	/* 左手 */
+		}
+	} else {
+		nCount = m_pPlayerChar->m_adwItemID.GetSize ();
+		for (i = 0; i < nCount; i ++) {
+			pInfoItem = (PCInfoItem)m_pLibInfoItem->GetPtr (m_pPlayerChar->m_adwItemID[i]);
+			if (pInfoItem == NULL) {
+				continue;
+			}
+			/* 選択している場所にアイテムがあった？ */
+			if ((pInfoItem->m_ptBackPack.x == ptPos.x) &&
+				(pInfoItem->m_ptBackPack.y == ptPos.y)) {
+				dwItemID = pInfoItem->m_dwItemID;
+			}
+			if (m_ptDrag.x != -1) {
+				/* ドラッグ開始位置のアイテム？ */
+				if ((pInfoItem->m_ptBackPack.x == m_ptDrag.x) &&
+					(pInfoItem->m_ptBackPack.y == m_ptDrag.y)) {
+					dwItemIDDrag = pInfoItem->m_dwItemID;
+				}
+			}
+		}
+		if (m_ptDrag.x != -1) {
+			/* ドラッグされた？ */
+			if (!((m_ptDrag.x == ptPos.x) && (m_ptDrag.y == ptPos.y))) {
+				m_ptDrop = ptPos;
+			}
+		}
+	}
+
+	if (dwItemIDDrag == 0) {
+		m_ptDrag.x = m_ptDrag.y = -1;
+		Redraw ();
+		if (dwItemID == 0) {
+			goto Exit;
+		}
+	}
+
+	m_dwSelectItemID = dwItemID;
+	m_dwDragItemID	 = dwItemIDDrag;
+	m_pMgrSound->PlaySound (SOUNDID_OK_PI73);
+	PostMessage (m_hWndMain, WM_WINDOWMSG, m_nID, dwItemID);
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::OnZ											 */
+/* 内容		:キーハンドラ(Z)												 */
+/* 日付		:2007/07/30														 */
+/* ========================================================================= */
+
+BOOL CWindowITEMMENU::OnZ(BOOL bDown)
+{
+	BOOL bRet;
+
+	bRet = FALSE;
+	if (bDown) {
+		goto Exit;
+	}
+
+	m_bDelete = TRUE;
+	m_pMgrSound->PlaySound (SOUNDID_CANCEL);
+
+	bRet = TRUE;
+Exit:
+	return bRet;
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::DrawEquip										 */
+/* 内容		:装備アイテムを描画												 */
+/* 日付		:2008/11/19														 */
+/* ========================================================================= */
+
+void CWindowITEMMENU::DrawEquip(
+	int nType,					/* [in] 種別 */
+	DWORD dwEquipItemID)		/* [in] 装備アイテムID */
+{
+	int x, y, anDrawInfo[] = {
+			1, 1, EQUIPTYPE_CLOTH,		/* 服 */
+			0, 0, EQUIPTYPE_ACCE1,		/* アクセサリ1 */
+			0, 1, EQUIPTYPE_ARMSRIGHT,	/* 右手 */
+			2, 1, EQUIPTYPE_ARMSLEFT,	/* 左手 */
+//			EQUIPTYPE_HEAD,			/* 頭 */
+//			EQUIPTYPE_ACCE2,		/* アクセサリ2 */
+//			EQUIPTYPE_CLOTH,		/* 服 */
+			0, 0, 0};
+	PCInfoItem pInfoItem;
+
+	if (dwEquipItemID == 0) {
+		return;
+	}
+	pInfoItem = (PCInfoItem)m_pLibInfoItem->GetPtr (dwEquipItemID);
+	if (pInfoItem == NULL) {
+		return;
+	}
+	x = 41 + 36 * anDrawInfo[nType * 3 + 0];
+	y = 10 + 36 * anDrawInfo[nType * 3 + 1];
+	m_pMgrDraw->DrawItem (m_pDib, x, y, pInfoItem);
+	if (m_nPos == anDrawInfo[nType * 3 + 2]) {
+		/* 装備中のアイテムなのでアイテム名を更新 */
+		m_strName = pInfoItem->m_strName;
+	}
+}
+
+
+/* ========================================================================= */
+/* 関数名	:CWindowITEMMENU::GetDrawPos									 */
+/* 内容		:アイテム描画位置を取得											 */
+/* 日付		:2008/11/19														 */
+/* ========================================================================= */
+
+void CWindowITEMMENU::GetDrawPos(
+	int nPos,		/* [in] アイテム位置 */
+	int &nDstX,		/* [out] X座標 */
+	int &nDstY)		/* [out] Y座標 */
+{
+	if (nPos < EQUIPTYPE_MAX) {
+		nDstX = 36 * (nPos % 3) + 41 + 1;
+		nDstY = 36 * (nPos / 3) + 10 + 1;
+	} else {
+		nDstX = 33 * ((nPos - EQUIPTYPE_MAX) % 5) + 11;
+		nDstY = 33 * ((nPos - EQUIPTYPE_MAX) / 5) + 96;
+	}
+}
+
+/* Copyright(C)URARA-works 2007 */
---
+++ tags/Verver0.030/SboCli/src/Window/WindowTEXTMSG.h
@@ -0,0 +1,69 @@
+/* Copyright(C)URARA-works 2008 */
+/* ========================================================================= */
+/* ファイル名：	WindowTEXTMSG.h												 */
+/* 内容：		テキストメッセージ表示ウィンドウクラス 定義ファイル			 */
+/* 作成：		年がら年中春うらら(URARA-works)								 */
+/* 作成開始日：	2008/11/22													 */
+/* ========================================================================= */
+
+#pragma once
+
+#include "WindowBase.h"
+
+class CImg32;
+class CMgrData;
+
+/* ========================================================================= */
+/* クラス宣言																 */
+/* ========================================================================= */
+
+typedef class CWindowTEXTMSG : public CWindowBase
+{
+public:
+			CWindowTEXTMSG();						/* コンストラクタ */
+	virtual ~CWindowTEXTMSG();						/* デストラクタ */
+
+	void	Create		(CMgrData *pMgrData);				/* 作成 */
+
+	void	Draw		(CImg32 *pDst);						/* 描画 */
+	BOOL	TimerProc	(void);								/* 時間処理 */
+	void	SetTitle	(LPCSTR pszTitle);					/* 肩書きを設定 */
+	void	SetName		(LPCSTR pszName);					/* 名前を設定 */
+	void	SetMsg		(LPCSTR pszMsg);					/* メッセージ設定 */
+
+
+protected:
+	BOOL OnUp			(void);								/* キーハンドラ(↑) */
+	BOOL OnDown			(void);								/* キーハンドラ(↓) */
+	BOOL OnLeft			(void);								/* キーハンドラ(←) */
+	BOOL OnRight		(void);								/* キーハンドラ(→) */
+	BOOL OnX			(BOOL bDown);						/* キーハンドラ(X) */
+	BOOL OnZ			(BOOL bDown);						/* キーハンドラ(Z) */
+	void DrawChar		(LPCSTR pszText);					/* １文字表示 */
+	void RenewTitle		(void);								/* 肩書と名前画像を更新 */
+	void InitText		(void);								/* メッセージ画像を初期化 */
+	void MsgProc		(void);								/* メッセージ解析処理 */
+	void GetLineText	(LPCSTR pszSrc, CmyString &strDst);	/* 1行取得 */
+	void TrimSpace		(CmyString &strSrc);				/* 前後の空白を除去 */
+	void GetBlock		(LPCSTR pszSrc, CmyString &strDst);	/* 大かっこ1ブロック分取得 */
+
+
+protected:
+	BOOL		m_bInputWait;				/* 入力待ち */
+	POINT		m_ptDraw;					/* 描画位置 */
+	int			m_nState,					/* 表示状態 */
+				m_nType,					/* ウィンドウ種別 */
+				m_nProcPos,					/* 処理中の文字位置 */
+				m_nProcPosTmp,				/* 処理中の文字位置 */
+				m_nSpaceHeight;				/* ウィンドウ上の余白の高さ */
+	DWORD		m_dwLastProc;				/* 前回の処理時間 */
+	CmyString	m_strTitle,					/* 肩書き */
+				m_strName,					/* 名前 */
+				m_strMsg,					/* メッセージ */
+				m_strMsgTmp;				/* メッセージ */
+	CmyStringArray	m_astrMenu;				/* メニュー項目 */
+	CImg32		*m_pDibTitle,				/* 肩書きと名前部分 */
+				*m_pDibText;				/* 文字描画テンポラリ */
+} CWindowTEXTMSG, *PCWindowTEXTMSG;
+
+/* Copyright(C)URARA-works 2008 */
