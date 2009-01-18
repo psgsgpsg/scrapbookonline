be8c1f484298f5a846fe594419a81db89c56783b
RXP10430 <RXP10430@15353e6c-9749-11dd-b35e-65caead18e0b>
2009-01-18 12:12:14 +0000

    2009/01/18版(ver0.035) ・[管理者ウィンドウ]「その他」→「アカウント」にアカウント追加画面を実装。 ・[管理者ウィンドウ]「キャラ」→「選択キャラの編集[所持スキル]」を実装。 ・NPCの索敵処理で2歩前で止まることがあったのを修正。 ・半キャラずれている状態で攻撃が当たるように修正。 ・「移動して攻撃」スキルで、4方向に発射できるように対応。

---
+++ tags/Verver0.035/Common/SBOVersion.h
@@ -0,0 +1,79 @@
+/* Copyright(C)URARA-works 2008 */
+/* ========================================================================= */
+/* ファイル名	:SBOVersion.h												 */
+/* 内容			:バージョン定義ファイル										 */
+/* 作成			:年がら年中春うらら(URARA-works)							 */
+/* 作成開始日	:2008/06/07													 */
+/* ========================================================================= */
+
+#pragma once
+
+/* ========================================================================= */
+/* 定数定義																	 */
+/* ========================================================================= */
+
+#define VERTEXT					"0.035"								/* バージョン */
+#define VERSIONVAL				(0x00003500)						/* バージョン番号 */
+#define DLLVER					"1"									/* DLLバージョン */
+#define GRPDATADLLVER			"4"									/* グラフィックデータDLLバージョン */
+
+/* ※注意[FILEVERSION]・[PRODUCTVERSION]を変更する必要があります */
+
+/****************************************************************************
+	定数の定義
+****************************************************************************/
+
+/* ファイルフラグ定数 */
+#ifdef _DEBUG
+#define FILEFLAGS_VALUE		(0x1L)
+#else
+#define	FILEFLAGS_VALUE		(0x0L)
+#endif
+
+/* ファイルタイプ定数 */
+#if(defined(_USRDLL) || defined(_WINDLL))
+#define	FILETYPE_VALUE		(0x2L)
+#else
+#define	FILETYPE_VALUE		(0x1L)
+#endif
+
+/****************************************************************************
+	バージョンリソース定義マクロ
+****************************************************************************/
+
+#define	SBO_VERSION(OriginalFilename)																	\
+																										\
+	VS_VERSION_INFO VERSIONINFO																			\
+		FILEVERSION 2,0,0,0																				\
+		PRODUCTVERSION 2,0,0,0																			\
+		FILEFLAGSMASK 0x3fL																				\
+		FILEFLAGS FILEFLAGS_VALUE																		\
+		FILEOS 0x40004L																					\
+		FILETYPE FILETYPE_VALUE																			\
+		FILESUBTYPE 0x0L																				\
+	BEGIN																								\
+		BLOCK "StringFileInfo"																			\
+		BEGIN																							\
+			BLOCK "041104b0"																			\
+			BEGIN																						\
+				VALUE "Comments", "スクラップブックオンライン\0"										\
+				VALUE "CompanyName", "URARA-WORKS\0"													\
+				VALUE "FileDescription", "スクラップブックオンライン\0"									\
+				VALUE "FileVersion", VERTEXT "\0"														\
+				VALUE "InternalName", " \0"																\
+				VALUE "LegalCopyright", "Copyright (C)2003-2009 URARA-WORKS. All rights reserved.\0"	\
+				VALUE "LegalTrademarks", "スクラップブックオンライン\0"									\
+				VALUE "OriginalFilename", OriginalFilename "\0"											\
+				VALUE "PrivateBuild", " \0"																\
+				VALUE "ProductName", "スクラップブックオンライン\0"										\
+				VALUE "ProductVersion", VERTEXT "\0"													\
+				VALUE "SpecialBuild", " \0"																\
+			END																							\
+		END																								\
+		BLOCK "VarFileInfo"																				\
+		BEGIN																							\
+			VALUE "Translation", 0x411, 1200															\
+		END																								\
+	END
+
+/* Copyright(C)URARA-works 2008 */
